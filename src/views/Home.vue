668522f42969403f346337746ef5bcd8ada23e7b
yuxinping <dm2267747196@qq.com>
2020-08-27 21:03:07 +0800

    搭建html结构

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="topnav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="banner">
+      <h1>轮子</h1>
+      <h2></h2>
+      <p>
+        <a>github</a>
+        <a>开始</a>
+      </p>
+    </div>
+  </div>
+</template>
