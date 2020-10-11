2d62bf91e2626b9da491aa4285a3f5216a791a8d
yuxinping <dm2267747196@qq.com>
2020-10-11 12:28:11 +0800

    topnav样式更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
 <div>
     <div class="topnav">
-        <div class="logo"></div>
-        <div class="menu"></div>
+        <div class="logo">logo</div>
+        <div class="menu">
+            <span style="margin-right:20px;">菜单1</span>
+            <span>菜单2</span>
+        </div>
     </div>
     <div class="banner">
         <h1>轮子</h1>
@@ -14,3 +17,17 @@
     </div>
 </div>
 </template>
+
+<style lang="scss">
+.topnav {
+    border: 1px solid red;
+    background: #efefef;
+    display: flex;
+    justify-content: space-between;
+
+    .logo,
+    .menu {
+        margin: 20px 40px 20px 40px;
+    }
+}
+</style>
