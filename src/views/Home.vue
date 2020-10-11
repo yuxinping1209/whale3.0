8d2b398ea376b420d32a531ec3369d9c84f9f6ed
yuxinping <dm2267747196@qq.com>
2020-10-11 12:07:27 +0800

    搭建html框架

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
-  <div>
+<div>
     <div class="topnav">
-      <div class="logo"></div>
-      <div class="menu"></div>
+        <div class="logo"></div>
+        <div class="menu"></div>
     </div>
     <div class="banner">
-      <h1>轮子</h1>
-      <h2></h2>
-      <p>
-        <a>github</a>
-        <a>开始</a>
-      </p>
+        <h1>轮子</h1>
+        <h2>一个厉害的UI框架</h2>
+        <p class="actions">
+            <a>github</a>
+            <a>开始</a>
+        </p>
     </div>
-  </div>
+</div>
 </template>
