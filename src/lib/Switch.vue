1595624880f08112916864e3ae5de217014ca1ac
yuxinping <dm2267747196@qq.com>
2021-01-04 21:46:32 +0800

    优化css

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -11,7 +11,7 @@
 		},
 		setup(props, context) {
 			const toggle = () => {
-				context.emit('input', !props.value)
+				context.emit('update:value', !props.value)
 			}
 
 			return {
@@ -28,28 +28,44 @@
 		height: $h;
 		width: $h*2;
 		border: none;
-		outline: none;
-		background: grey;
+		background: #bfbfbf;
 		border-radius: $h/2;
 		position: relative;
-	}
 
-	span {
-		position: absolute;
-		top: 2px;
-		left: 2px;
-		height: $h2;
-		width: $h2;
-		background: white;
-		border-radius: $h2 / 2;
-		transition: left 250ms;
-	}
+		>span {
+			position: absolute;
+			top: 2px;
+			left: 2px;
+			height: $h2;
+			width: $h2;
+			background: white;
+			border-radius: $h2 / 2;
+			transition: all 250ms;
+		}
 
-	button.checked {
-		background: blue;
-	}
+		&.checked {
+			background: #1890ff;
+		}
 
-	button.checked>span {
-		left: calc(100% - #{$h2} - 2px);
+		&.checked>span {
+			left: calc(100% - #{$h2} - 2px);
+		}
+
+		&:focus {
+			outline: none;
+		}
+
+		&:active {
+			>span {
+				width: $h2 + 4px;
+			}
+		}
+
+		&.checked:active {
+			>span {
+				width: $h2 + 4px;
+				margin-left: -4px;
+			}
+		}
 	}
 </style>
